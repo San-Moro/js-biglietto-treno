66dce68dd270da3c724b3befc82fd81badb96980
San-Moro <valentinacortese.dsg@gmail.com>
2022-10-06 17:07:24 +0200

    Added input and processing

--- js/script.js
+++ js/script.js
@@ -5,10 +5,19 @@
 // va applicato uno sconto del 40% per gli over 65.
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 
+// INPUT
 // Chiedi all’utente i km
+const km = parseInt(prompt("Quanti km devi percorrere?"));
 // poi chiedi age
+const age = parseInt(prompt("Quanti anni hai?"));
 // Convertire i due valori in numeri
+console.log(km, typeof(km));
+console.log(age, typeof(age));
+
+// PROCESSING
 // calcola prezzo biglietto intero => 0.21€/km
+const basicTicket = km * 0.21;
+console.log(basicTicket);
 
 // SE age < 18
 //     - sconto del 20%
@@ -17,4 +26,23 @@
 // Altrimenti
 //     -prezzo biglietto intero
 
+let price = "";
+
+if (age < 18) {
+    // sconto del 20%;
+    let discountSmall = basicTicket * 0.2;
+    // total price
+    price = basicTicket - discountSmall;
+    console.log(price);
+} else if (age > 65) {
+    // sconto del 40%;
+    let discountBig = basicTicket * 0.4;
+    // total price
+    price = basicTicket - discountBig;
+    console.log(price);
+} else {
+    basicTicket;
+}
+
+// OUTPUT
 // Output => con massimo 2 decimali (-vedi documentazione tra preferiti-)
